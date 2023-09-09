d7874c0c1922c41e57a3bad23ec223f177c55455
Manuel De Leon <manuel@deleonnet.com>
2023-09-09 13:15:31 -0300

    Routes configurations done

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './components/Layout'
+import NuevoCliente from './pages/NuevoCliente'
+import Index, { loader as clientesLoader } from './pages' //el loader es para gestionar el state con React Router DOM
 import './index.css'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <h1>Inicio</h1>
+    element: <Layout/>,
+    children: [
+      {
+        index: true,
+        element: <Index/>,
+        loader: clientesLoader,
+      },
+      {
+        path: '/clientes/nuevo',
+        element: <NuevoCliente/>
+      }
+    ]
   }
 ])
 
---
+++ src/pages/NuevoCliente.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NuevoCliente = () => {
+  return (
+    <div>
+      Nuevo CLiente
+    </div>
+  )
+}
+
+export default NuevoCliente
